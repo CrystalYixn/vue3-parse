7ab99cf0c32c69aa72da1eb0a931499546001ee1
Honi <1095175021@qq.com>
2023-09-30 19:36:01 +0800

    feat: 依赖收集的实现

--- packages/reactivity/src/baseHandlers.ts
+++ packages/reactivity/src/baseHandlers.ts
@@ -1,3 +1,5 @@
+import { track, trigger } from "./effect"
+
 export const enum ReactiveFlags {
   IS_REACTIVE = '__v_isReactive'
 }
@@ -5,13 +7,19 @@ export const enum ReactiveFlags {
 export const mutableHandler = {
   get(target, key, receiver) {
     if (key === ReactiveFlags.IS_REACTIVE) return true
+    track(target, 'get', key)
     // 会在属性取值时将 this 指向改为 receiver
     return Reflect.get(target, key, receiver)
     // 不能使用, 如果 target[key] 的值是一个访问器读取了 this 会导致无法监听
     // return target[key]
   },
   set(target, key, val, receiver) {
+    const oldValue = target[key]
     // 返回一个布尔值声明赋值操作是否成功
-    return Reflect.set(target, key, val, receiver)
+    const result = Reflect.set(target, key, val, receiver)
+    if (result && oldValue !== val) {
+      trigger(target, 'set', key, val, oldValue)
+    }
+    return result
   }
 }
--- packages/reactivity/src/effect.ts
+++ packages/reactivity/src/effect.ts
@@ -1,8 +1,12 @@
-export let activeEffect = null
+export let activeEffect: ReactiveEffect | null = null
 
 class ReactiveEffect {
   // ts 写法, 要求在 constructor 前在外边定义
   active = true
+  parent = null
+  // 需要双向记忆属性
+  // 如 flag ? this.name : this.age 切换 flag 时清除另一方的收集使用
+  deps = []
   // ts 写法, 会自动将 fn 挂载到 this 上
   constructor(public fn) { }
 
@@ -11,15 +15,52 @@ class ReactiveEffect {
     // 非激活状态只需要执行函数, 不进行依赖收集
     if (!this.active) return this.fn()
     try {
+      this.parent = activeEffect
       activeEffect = this
       return this.fn()
     } finally {
-      activeEffect = null
+      // 很多东西没必要放在全局, 对象相关的流程逻辑尽量封装到对象内部实现
+      // 从栈结构转换为了树结构, 减少全局污染
+      // 每个对象只关心他的父亲而不关心栈里的其他 Effect , 所以逻辑更清晰
+      activeEffect = this.parent
+      this.parent = null
     }
   }
 }
+
+const targetMap = new WeakMap<object, Map<string, Set<ReactiveEffect>>>()
 
 export function effect(fn) {
   const _effect = new ReactiveEffect(fn)
   _effect.run()
 }
+
+export function track(target, type, key) {
+  if (!activeEffect) return
+  let depsMap = targetMap.get(target)
+  if (!depsMap) {
+    targetMap.set(target, (depsMap = new Map()))
+  }
+  let dep = depsMap.get(key)
+  if (!dep) {
+    depsMap.set(key, (dep = new Set()))
+  }
+  const shouldTrack = !dep.has(activeEffect)
+  if (shouldTrack) {
+    dep.add(activeEffect)
+    activeEffect.deps.push(dep)
+  }
+}
+
+export function trigger(target, type, key, value, oldValue) {
+  const depsMap = targetMap.get(target)
+  // 未被收集的响应式属性不处理
+  if (!depsMap) return
+  const effects = depsMap.get(key)
+  effects?.forEach(e => {
+    // 过滤在 effect 中重新触发当前 effect 的情况
+    e !== activeEffect && e.run()
+  })
+}
+
+console.log(targetMap)
