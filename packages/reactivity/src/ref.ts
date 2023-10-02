1bb7f80863c8e6397774dff3730371cc794ce3f9
Honi <1095175021@qq.com>
2023-10-02 19:08:52 +0800

    feat: proxyRefs的实现

--- packages/reactivity/src/ref.ts
+++ packages/reactivity/src/ref.ts
@@ -51,3 +51,23 @@ export function toRefs(object) {
   }
   return obj
 }
+
+/** 自动访问 .value, 一般 Vue 在模板阶段自动使用 with 包裹少写一部分内容 */
+export function proxyRefs(object) {
+  return new Proxy(object, {
+    get(target, key, receiver) {
+      const r = Reflect.get(target, key, receiver).value
+      return r.__v_isRef ?  r.value : r
+    },
+    set(target, key, newValue, receiver) {
+      const oldValue = Reflect.get(target, key, receiver)
+      let res = false
+      if (oldValue.__v_isRef) {
+        res = Reflect.set(oldValue, 'value', newValue, receiver)
+      } else {
+        res = Reflect.set(target, key, newValue, receiver)
+      }
+      return res
+    }
+  })
+}
