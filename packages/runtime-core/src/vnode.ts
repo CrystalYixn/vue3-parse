be234e30a26e7a85c07bf359faa302510b679383
Honi <1095175021@qq.com>
2023-10-03 02:59:46 +0800

    feat: h方法和createVnode的实现

---
+++ packages/runtime-core/src/vnode.ts
@@ -0,0 +1,33 @@
+import { ShapeFlag, isArray, isString } from "@vue/shared";
+
+export function isVnode(value) {
+  return !!(value?.__v_isVnode)
+}
+
+export function createVnode(type, props, children = null) {
+  // 组合方案 shapeFlag, 利用位二进制和运算符可以快速进行类型的组合与包含判断
+  let shapeFlag = isString(type) ? ShapeFlag.ELEMENT : 0
+  const vnode = {
+    type,
+    props,
+    children,
+    el: null,
+    __v_isVnode: true,
+    shapeFlag,
+  }
+  if (children) {
+    let type = 0
+    if (isArray(children)) {
+      type = ShapeFlag.ARRAY_CHILDREN
+    } else {
+      children = String(children)
+      type = ShapeFlag.TEXT_CHILDREN
+    }
+    // 用一个值来表示多个语义, 此处表示 tag 的类型和孩子的类型
+    // 原理是: 值作为一个二进制数据, 每一种类型都只将一位变成 1
+    // 这样通过二进制运算符对指定位进行判断即可得出值是否包含某个类型
+    vnode.shapeFlag |= type
+  }
+
+  return vnode
+}
--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -23,3 +23,17 @@ export const isNumber = (val: unknown): val is number => {
 export const isNullish = (val: unknown): val is undefined|null => {
   return typeof val === 'undefined' || typeof val === null
 }
+
+export const enum ShapeFlag {
+  ELEMENT = 1,
+  FUNCTIONAL_COMPONENT = 1 << 1,
+  STATEFUL_COMPONENT = 1 << 2,
+  TEXT_CHILDREN = 1 << 3,
+  ARRAY_CHILDREN = 1 << 4,
+  SLOTS_CHILDREN = 1 << 5,
+  TELEPORT = 1 << 6,
+  SUSPENSE = 1 << 7,
+  COMPONENT_SHOULD_KEEP_ALIVE = 1 << 8,
+  COMPONENT_KEPT_ALIVE = 1 << 9,
+  COMPONENT = ShapeFlag.STATEFUL_COMPONENT | ShapeFlag.FUNCTIONAL_COMPONENT,
+}
